4ce9bde70989b24d7e9569a48b89fa239630510c
Uhhha <1226264188@qq.com>
2021-06-24 11:52:57 +0800

    修复XSS注入漏洞

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Observers\TopicObserver;
+use App\Models\Topic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Topic::observe(TopicObserver::class);
     }
 }
